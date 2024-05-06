68480c4af5c5ab820c9131ea7998af19cb8cbc20
bautistagorchs <bautista.gorchs@gmail.com>
2024-05-06 19:28:48 -0300

    remove tailwind and add Sass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl text-amber-500">
-        Hola soy un proyecto con Tailwind
-      </h1>
-    </main>
-  );
+  return <h1>House of Dev</h1>;
 }
