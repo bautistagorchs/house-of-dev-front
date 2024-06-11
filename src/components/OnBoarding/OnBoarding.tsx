fcad31a9b4f53ecfe301ab5f337386644795d870
bautistagorchs <bautista.gorchs@gmail.com>
2024-06-11 17:23:43 -0300

    change on boarding view

--- src/components/OnBoarding/OnBoarding.tsx
+++ src/components/OnBoarding/OnBoarding.tsx
@@ -2,30 +2,31 @@ import React from "react";
 import s from "./OnBoarding.module.scss";
 import Image from "next/image";
 import logo from "../../assets/logo.png";
-import vector from "../../assets/Vector.png";
-import arrow from "../../assets/Arrow 14.png";
+import vectorUndraw from "../../assets/vectorUndraw.svg";
+import funArrowUndraw from "../../assets/funArrowUndraw.svg";
 import Link from "next/link";
 
 const OnBoarding = () => {
   return (
-    <Link href={"/login"}>
-      <div className={s.outerContainer}>
-        <div className={s.redBg}>
-          <div className={s.logoContainer}>
-            <Image src={logo} alt="logo" />
-          </div>
-          <div className={s.vectorContainer}>
-            <Image src={vector} alt="vector" />
-            <h3 className={s.slogan}>Tu nueva vivienda está aqui</h3>
-          </div>
+    <div className={s.outerContainer}>
+      <div className={s.redBg}>
+        <div className={s.logoContainer}>
+          <Image src={logo} alt="logo" />
         </div>
-        <div className={s.arrowsContainer}>
-          <Image src={arrow} alt="arrow" />
-          <Image src={arrow} alt="arrow" />
-          <Image src={arrow} alt="arrow" />
+        <div className={s.vectorContainer}>
+          <h3 className={s.slogan}>Tu nueva vivienda está aqui</h3>
+          <Image src={vectorUndraw} alt="vector" height={85} />
         </div>
       </div>
-    </Link>
+      <div className={s.arrowsContainer}>
+        <Image src={funArrowUndraw} alt="arrow" height={180} />
+      </div>
+      <div className={s.buttonContainer}>
+        <button>
+          <Link href={"/login"}>Comenzar</Link>
+        </button>
+      </div>
+    </div>
   );
 };
 
